e72e740f2c368f008000c6b48a1f7f31bcbd3ebe
Kris Rawson <Kris.Rawson@gmail.com>
2021-08-05 11:10:50 -0600

    C# "Controling Program Flow" demo app built out through the end of the third module.

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WiredBrainCoffeeSurveys.Reports
 {
@@ -6,7 +7,66 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            GenerateTasksReport();
+
+        }
+
+        public static void GenerateTasksReport()
+        {
+            var tasks = new List<string>();
+            // Calculated Values
+            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+            //double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
+            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
+
+            // Logical Comparisons
+            bool isCoffeeScoreLower = Q1Results.CoffeeScore < Q1Results.FoodScore;
+
+            if (Q1Results.CoffeeScore < Q1Results.FoodScore)
+            {
+                tasks.Add("Investigate coffee recipes and ingredients.");
+            }
+
+            if (overallScore > 8.0)
+            {
+                tasks.Add("Work with leadership to reward staff");
+            }
+            else
+            {
+                tasks.Add("Work with employees for improvement ideas.");
+            }
+
+            if (responseRate < .33)
+            {
+                tasks.Add("Research options to improve response rate.");
+            }
+            else if (responseRate > .33 && responseRate < .66)
+            {
+                tasks.Add("Reward participants with free coffee coupon.");
+            }
+            else
+            {
+                tasks.Add("Reward participants with discount coffee coupon.");
+            }
+
+            switch (Q1Results.AreaToImprove)
+            {
+                case "RewardsProgram":
+                    tasks.Add("Revist the Rewards deals.");
+                    break;
+
+                case "Cleanliness":
+                    tasks.Add("Contact the cleaning vendor.");
+                    break;
+
+                case "MobileApp":
+                    tasks.Add("Contact consulting firm about app.");
+                    break;
+
+                default:
+                    tasks.Add("Investigate individual comments for ideas.");
+                    break;
+            }
         }
     }
 }
